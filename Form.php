7481a07abc702b692eb5341c2a2ae72a151e9831
fadhilrazzan <54299052+fadhilrazzan@users.noreply.github.com>
2020-03-09 10:08:43 +0700

    Add files via upload

---
+++ Form.php
@@ -0,0 +1,53 @@
+
+<head>
+<title>Form</title>
+<body>
+    <form action = "" method ="post">
+<p>
+   <label>Nama Depan</label>
+   <input type ="text" Nama="...." placeholder>
+</p>
+<p>
+<label>Nama Belakang</label>
+   <input type ="text" Nama="...." placeholder>  
+</p>
+<p>
+    <label>NISN</label>
+    <input type ="text" NISN="...." placeholder>
+</p>
+<p>
+<label>Tanggal & Tempat Lahir</label>
+   <input type ="text" TTL="...." placeholder>
+</p>
+<P>
+<label>Agama</label>
+   <input type ="text" Agama="...." placeholder>
+</p>
+<p>
+<label>Jenis Kelamin</label>
+<input type='radio' name='jenis_kelamin' value='Pria' />Pria 
+<input type='radio' name='jenis_kelamin' value='Wanita' />Perempuan
+</p>
+<p>
+<label>Email</label>
+   <input type ="text" Email="siapa@gmail.com" placeholder>
+</p>
+<p>
+<label>Telepon</label>
+<input type ="text" Telepon="+62.." placeholder>
+</p>
+<p>
+<select name="Jurusan"><option value="Rekayasa Perangkat Lunak">Rekayasa Perangkat Lunak</option><option value="Teknik Komputer">Teknik Komputer</option></select>
+</p>
+<p>
+<select name="Jurusan"><option value="A">Rekayasa Perangkat Lunak</option><option value="B">Teknik Komputer</option></select>
+<p>
+<label>Username</label>
+   <input type ="text" Username="...." placeholder>
+   <p>
+   <label>Password</label>
+   <input type ="text" Password="...." placeholder>
+</p>
+<p>
+<button type="submit" class="btn btn-success" onclick="convert();" style="height: 100%; width: 10%"></button>
+</p>
